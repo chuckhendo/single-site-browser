812a57a07df8174107be60c61ad74ae70c530a75
Carl Henderson <chuckhendo@gmail.com>
2019-07-04 21:09:10 -0400

    deletes unneeded webview.ts

--- src/electron/webview.ts
+++
@@ -1,7 +0,0 @@
-// window.onbeforeunload = function(e) {
-//   return 'Dialog text here.';
-// };
-console.log('wat');
-window.addEventListener('onbeforeunload', () => {
-  console.log('MOVING');
-});
